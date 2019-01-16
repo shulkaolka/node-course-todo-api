a1c7ded6932cd235fd65266db0cd73463ebbcc87
Olha Virolainen <olga.shukel@gmail.com>
2019-01-16 17:26:15 +0200

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp')
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5c3f3dcb5ded67b012a06431')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos')
+  // .find()
+  // .count()
+  // .then((count) => {
+  //   console.log(`Todos count ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users')
+  .find({name: 'Olha'})
+  .count()
+  .then((count) => {
+    console.log(`Todos count ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  client.close();
+});
