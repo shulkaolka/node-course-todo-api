aeaf158cd8724abce0911384cf1700a230ef015c
Olha Virolainen <olga.shukel@gmail.com>
2019-01-17 11:52:55 +0200

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5c3f16a2dc63df7bd9f86672')
+  // },{
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5c3f4abcfe0e601b503663e2')
+  },{
+    $set: {
+      name: 'Olha'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  client.close();
+});
