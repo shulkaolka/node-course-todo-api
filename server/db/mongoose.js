cd03d11b6aa09044676d7b5afaf7bf6396aea2d1
Olha Virolainen <olga.shukel@gmail.com>
2019-01-17 17:26:35 +0200

    Change default MONGO_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
-var MONGO_URI = process.env.MONGO_URI || 'mongodb://herokuuser:qweASD123@ds159624.mlab.com:59624/todosapps'
+var MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp'
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
-
 module.exports = {mongoose}
