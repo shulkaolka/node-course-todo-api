e336aa6774cc86787e940e91477b22d6a88102b7
Olha Virolainen <olga.shukel@gmail.com>
2019-01-18 16:31:51 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,29 @@
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
-var token = jwt.sign(data, '123');
-console.log(token);
+var password = '123asd!';
 
-var decoded = jwt.verify(token, '123');
-console.log(decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
+
+var hashedPassword = '$2a$10$aErtjlHjDH.BuGKsfgVgaOYKgcHDxitgHCzG2klRcv12FkKvdwNxS';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+
+
+// const jwt = require('jsonwebtoken');
+
+// var data = {
+//   id: 10
+// };
+// var token = jwt.sign(data, '123');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123');
+// console.log(decoded);
 
 // const {SHA256} = require('crypto-js');
 // var message = 'I';
